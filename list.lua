83b9121d2f1497e3a65b15a319ff2abb3e81140b
TheLuaFox86 <peanuts005@outlook.com>
2025-12-28 22:00:17 +0000

    Update List To Add New Music

--- list.lua
+++ list.lua
@@ -1,7 +1,7 @@
 return {{
     name="Paranoid",
     author="Black Sabath",
-    path="P-BS.dfpwm",
+    path="MainMusic/P-BS.dfpwm",
   }, {
     name="Bet On Me",
     author="Walk Off The Earth",
@@ -22,7 +22,18 @@ return {{
   },
   {
     name="Golden",
-    author="HunterX",
+    author="HunteriX",
     path="Pop/G-HX.dfpwm"
+    genre="pop",
+  },
+  {
+    name="Gods Plan",
+    author="Drake",
+    path="MainMusic/GP-D.dfpwm,
+  }
+  {
+    name="Crash",
+    author="Loud Luxury"
+    path="Pop/C-LL.dfpwm"
   }
 }
